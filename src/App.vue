f860c32a1537238599a5a8c7240f3db667d9d169
hucongcong <flycc258@163.com>
2020-01-14 10:09:13 +0800

    项目的样式处理

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,20 @@
 <template>
   <div id="app">
-    app
+    <div class="demo">
+      中国
+      <i class="iconfont iconwode"></i>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
-
+.demo {
+  width: 200px;
+  height: 200px;
+  background-color: pink;
+  font-size: 24px;
+  i {
+    font-size: 24px;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入lib-flexible
+import 'lib-flexible'
+
+// 导入通用的样式
+import './styles/base.css'
+// 导入字体图标文件
+import './styles/iconfont.css'
+
 Vue.config.productionTip = false
 
 new Vue({
